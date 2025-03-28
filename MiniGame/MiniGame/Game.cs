6950ff5fc5409496aab8c63cc7ec87e9fd76ffd5
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2025-03-28 16:10:18 +0300

    feat: load from file

--- MiniGame/MiniGame/Game.cs
+++ MiniGame/MiniGame/Game.cs
@@ -2,5 +2,20 @@ namespace MiniGame;
 
 public class Game
 {
-    
+      /// <summary>
+      /// Read.
+      /// </summary>
+      /// <param name="filename">Name of file.</param>
+      /// <returns>Array element of maze.</returns>
+      public char[][] Read(string filename)
+      {
+            string[] line = File.ReadAllLines(filename);
+            char[][] maze = new char[line.Length][];
+            for (int i = 0; i < line.Length; ++i)
+            {
+                  maze[i] = line[i].ToCharArray();
+            }
+
+            return maze;
+      }
 }
