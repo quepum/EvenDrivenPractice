77a38639e89a588a207dbe20058f5fbe6ecf24e6
quepum <118292850+quepum@users.noreply.github.com>
2025-03-28 16:11:25 +0300

    feat: add class EventLoop

---
+++ MiniGame/MiniGame/EventLoop.cs
@@ -0,0 +1,36 @@
+﻿namespace MiniGame;
+
+public class EventLoop
+{
+    private readonly Game game;
+
+    public EventLoop(Game game)
+    {
+        game = game;
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            var key = Console.ReadKey();
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    game.RunGame('L');
+                    break;
+                case ConsoleKey.RightArrow:
+                    game.RunGame('R');
+                    break;
+                case ConsoleKey.UpArrow:
+                    game.RunGame('U');
+                    break;
+                case ConsoleKey.DownArrow:
+                    game.RunGame('D');
+                    break;
+                case ConsoleKey.Escape:
+                    return;
+            }
+        }
+    }
+}
