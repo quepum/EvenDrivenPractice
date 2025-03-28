d9a51a3b43a564af04e3dea4110e3739d0bd0dfa
AV-Bog <arinabogdanova1967@mail.ru>
2025-03-28 16:14:17 +0300

    add RunGame and FindPlayerPosition

--- MiniGame/MiniGame/Game.cs
+++ MiniGame/MiniGame/Game.cs
@@ -2,5 +2,58 @@ namespace MiniGame;
 
 public class Game
 {
-    
+    private static char[,] map;
+    private static int playerX, playerY;
+    private static int mapWidth, mapHeight;
+    static void RunGame(char direction)
+    {
+        Console.Clear();
+        
+        while (true)
+        {
+            int newX = playerX, newY = playerY;
+
+            char key = Console.ReadKey().KeyChar;
+            switch (key)
+            {
+                case 'U':
+                    newY--;
+                    break;
+                case 'D':
+                    newY++;
+                    break;
+                case 'L':
+                    newX--;
+                    break;
+                case 'R':
+                    newX++;
+                    break;
+                default:
+                    continue;
+            }
+            
+            //вызов делегата для обработки движения 
+        }
+    }
+
+    static void FindPlayerPosition()
+    {
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                if (map[x, y] == '@')
+                {
+                    playerX = x;
+                    playerY = y;
+                    return;
+                }
+
+                playerX = 1;
+                playerY = 1;
+                
+                return;
+            }
+        }
+    }
 }
